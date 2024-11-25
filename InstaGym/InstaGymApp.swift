cc629c568ffde3c2ea6f4452d1c4aa3e9e1df99a
Rodrigo Casado <rodrigo.casado@ethon.ai>
2024-11-25 22:55:31 +0100

    Initial Commit

---
+++ InstaGym/InstaGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaGymApp.swift
+//  InstaGym
+//
+//  Created by Rodrigo Casado on 25.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
